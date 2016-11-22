4fc63e55967a14ea6b17124cf26f1a0d524c80e0
tjjj <wliu53@uic.edu>
2016-11-22 17:13:41 -0600

    init

---
+++ web/resources/js/helpers/utils.js
@@ -0,0 +1,20 @@
+// APP FIREBASE URL
+var root = 'https://happy-life-5c492.firebaseio.com/';
+
+
+/*
+* Helper Utilities
+*
+*/
+
+var utils = {
+  urls: {
+    root: root,
+    furniture: root + 'furniture/',
+    background: root + 'background/'
+  }
+};
+
+
+// EXPORT MODULE
+module.exports = utils;
